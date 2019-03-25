94bb680bca297ce1cb96be2d096f47adca1fdf81
Dharani1799 <46838677+Dharani1799@users.noreply.github.com>
2019-03-25 06:44:16 +0530

    p124.c

---
+++ p124.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+    int n,a[20],i,j,c=0,flag=0;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+	for(i=2;i<1000;i++)
+	{
+	    c=0;
+	    for(j=0;j<n;j++)
+	    {
+	        if(i%a[j]==0)
+	        {
+	            c++;
+	        }
+	        else
+	        {
+	            break;
+	        }
+	    }
+	    if(c==n)
+	    {
+	        printf("%d is the common ",i);
+	        flag=1;
+	        break;
+	    }
+	}
+	if(flag==0)
+	{
+	    printf("no");
+	}
+	
+}
